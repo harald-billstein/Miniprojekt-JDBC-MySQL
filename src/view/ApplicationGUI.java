c1cf86c880e10fdf390da1130093f7728a776906
Harald Billstein <harald.billstein@gmail.com>
2017-10-06 23:25:03 +0200

    init GUI

---
+++ src/view/ApplicationGUI.java
@@ -0,0 +1,13 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ApplicationGUI extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
