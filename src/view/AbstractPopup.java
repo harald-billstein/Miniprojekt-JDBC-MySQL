5ebf4a81de4226569ec7d33cd06c0f5ac98d8822
CRLON <cristofferlonn@gmail.com>
2017-10-24 14:27:56 +0200

    Added Javadocs for view classes. Removed some unused code.

--- src/view/AbstractPopup.java
+++ src/view/AbstractPopup.java
@@ -3,22 +3,39 @@ package view;
 import controller.TheFirmController.Observers;
 import javafx.scene.control.Button;
 
+/**
+ * This abstract class creates a confirmbutton, a cancelbutton and sets observers.
+ * @author Cristoffer
+ * @author Harald
+ */
+
 abstract class AbstractPopup implements PopupInterface {
 
   private Button cancelButton, confirmButton;
-  private String confirmButtonText, cancelButtonId, confirmButtonId;
+  private String confirmButtonText, confirmButtonId;
   private Observers observers;
   private final static int PREFERRED_BUTTON_WIDTH = 100;
 
-  AbstractPopup(String cancelButtonId, String confirmButtonText,
+  /**
+   * @param confirmButtonText Text to show on confirm-button
+   * @param confirmButtonId ID to set on the confirm-button
+   * @param observers Inner class Observers in Controller to use with this class
+   */
+
+  AbstractPopup(String confirmButtonText,
       String confirmButtonId, Observers observers) {
     this.confirmButtonText = confirmButtonText;
-    this.cancelButtonId = cancelButtonId;
     this.confirmButtonId = confirmButtonId;
     this.observers = observers;
     createCancelButton();
     createConfirmButton();
   }
+
+  /**
+   * Creates a confirm button with text and id specified in the constructor.
+   * Sets width of button
+   * Sets which type of event used by the button
+   */
 
   public void createConfirmButton() {
     confirmButton = new Button(confirmButtonText);
@@ -27,21 +44,35 @@ abstract class AbstractPopup implements PopupInterface {
     confirmButton.setOnAction(observers.getActionEvent());
   }
 
+  /**
+   * Creates a cancel button.
+   * Sets width of button
+   */
   public void createCancelButton() {
     cancelButton = new Button("Cancel");
     cancelButton.setMinWidth(PREFERRED_BUTTON_WIDTH);
-    //TODO: REMOVE? MAYBE NOT USED
-    cancelButton.setId(cancelButtonId);
   }
 
+  /**
+   * Getter for the cancel button.
+   * @return This cancel button object
+   */
   Button getCancelButton() {
     return cancelButton;
   }
 
+  /**
+   * Getter for the confirm button.
+   * @return This confirm button object
+   */
   Button getConfirmButton() {
     return confirmButton;
   }
 
+  /**
+   * Sets the observers for this class.
+   * @param observers Inner class of observers in TheFirmController
+   */
   public void setObserver(Observers observers) {
     this.observers = observers;
   }
